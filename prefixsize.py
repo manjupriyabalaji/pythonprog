017d85ea90d0e4ed50c1315a609ebb150b60b460
manjupriyabalaji <52314144+manjupriyabalaji@users.noreply.github.com>
2019-07-15 10:31:44 +0530

    Create prefixsize.py

---
+++ prefixsize.py
@@ -0,0 +1,23 @@
+x = int(input())
+strn1 = []
+for y in range(0,x):
+    strn = input()
+    strn1.append(strn)
+
+y = 0
+z = 0
+flag = True
+for b in range(0,len(strn1[0])):
+    if(flag==False):
+        break
+    s=1
+    while(s<a and strn1[0][b]==strn1[s][y]):
+        s+=1
+    if(s==x):
+        z+=1
+    else:
+        flag = False
+        break
+    
+for y in range(0,z):
+    print(strn1[0][y],end="")
